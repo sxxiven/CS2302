08bd5f1f2f5482fd00a4ad2f2067470742a00087
sxxiven <47461347+sxxiven@users.noreply.github.com>
2020-09-28 17:31:51 -0600

    Add files via upload

---
+++ iter.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+<h1>Iteration Program</h1>
+Today's date:
+<?php echo date("l F d, Y"); ?>
+<h3>Enter a Value to Iterate</h3>
+<form action="results.php" method=post>
+<input type="text" name="data">
+<p/>
+<input type="submit" value="Show Results">
+</form>
+</body>
+</html>
